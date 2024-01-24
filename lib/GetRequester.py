e5573e8e6a339bc772239890242c4a791717977e
33-66 <simon.nganga@student.moringaschool.com>
2024-01-24 15:07:37 +0300

    work done

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,17 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)  # Corrected the method name from ge to get
+        return response.content
 
     def load_json(self):
-        pass
+        data = []
+        info = json.loads(self.get_response_body())
+        for each_info in info:
+            data.append({"name": each_info["name"], "occupation": each_info["occupation"]})
+        return data
+
+get_requester = GetRequester('https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json')
+print(get_requester.load_json())
+# Output:
+# [{'name': 'Daniel', 'occupation': 'LG Fridge Salesman'}, {'name': 'Joe', 'occupation': 'WiFi Fixer'}, {'name': 'Avi', 'occupation': 'DJ'}, {'name': 'Howard', 'occupation': 'Mountain Legend'}]
